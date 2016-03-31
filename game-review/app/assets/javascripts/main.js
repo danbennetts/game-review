304cf5bf3768b2d768d0ab6738daff0d826ab0ef
Daniel Bennetts <daniel.bennetts@bigpond.com>
2016-03-31 22:20:04 +1100

    fixed some css styling

--- game-review/app/assets/javascripts/main.js
+++ game-review/app/assets/javascripts/main.js
@@ -5,17 +5,17 @@ $(window).load(function(){
  })
 
 $(document).ready(function () {
-   $(document).on("click", "[data-behaviour='delete']", function (event) {
+   $(document).on("click", "[data-behaviour='game-delete']", function (event) {
       event.preventDefault();
 
       swal({
          title: 'Are you sure?',
-         text: 'Once you delete this game/review you will not be able to retrieve it again.',
+         text: 'Once you delete this game there is no going back.',
          type: 'warning',
          showCancelButton: true,
          confirmButtonColor: '#DD6B55',
          confirmButtonText: 'Yes, delete it!',
-         cancelButtonText: 'No, cancel plx!',
+         cancelButtonText: 'No, cancel please!',
          closeOnConfirm: true,
          closeOnCancel: true
       }, function (confirmed) {
@@ -25,7 +25,41 @@ $(document).ready(function () {
                dataType: "JSON",
                method: "DELETE",
                success: function () {
-                  swal( 'Deleted!', 'success' );
+                  swal( 'Deleted!', 'Your request was a success!', 'success' );
+                  // window.location.href = "/";
+                  window.location.reload(true);
+                  window.location = window.location.origin + "?nocache=true";
+               }
+            });
+         }
+
+      });
+
+   });
+});
+
+$(document).ready(function () {
+   $(document).on("click", "[data-behaviour='review-delete']", function (event) {
+      event.preventDefault();
+
+      swal({
+         title: 'Are you sure?',
+         text: 'Once you delete this review there is no going back.',
+         type: 'warning',
+         showCancelButton: true,
+         confirmButtonColor: '#DD6B55',
+         confirmButtonText: 'Yes, delete it!',
+         cancelButtonText: 'No, cancel please!',
+         closeOnConfirm: true,
+         closeOnCancel: true
+      }, function (confirmed) {
+         if (confirmed) {
+            $.ajax({
+               url: $(this).attr("href"),
+               dataType: "JSON",
+               method: "DELETE",
+               success: function () {
+                  swal( 'Deleted!', 'Your request was a success!', 'success' );
                   // window.location.href = "/";
                   window.location.reload(true);
                   window.location = window.location.origin + "?nocache=true";
